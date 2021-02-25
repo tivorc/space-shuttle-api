6be3368fbcfd15dcdcc2c62bbb954ed7d8f640f6
Victor Gil <victoredin94@hotmail.com>
2021-02-25 10:40:25 -0500

    initial commit

---
+++ api/Controllers/SpaceShuttleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Space.Domain;
+
+namespace Api.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class SpaceShuttleController : ControllerBase
+  {
+    private readonly ILogger<SpaceShuttleController> _logger;
+
+    public SpaceShuttleController(ILogger<SpaceShuttleController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<SpaceShuttle> Get()
+    {
+      var list = new List<SpaceShuttle>(){
+        new SpaceShuttle() {UID = System.Guid.NewGuid().ToString(), Name = "Columbia"},
+        new SpaceShuttle() {UID = System.Guid.NewGuid().ToString(), Name = "Challenger"},
+        new SpaceShuttle() {UID = System.Guid.NewGuid().ToString(), Name = "Discovery"},
+        new SpaceShuttle() {UID = System.Guid.NewGuid().ToString(), Name = "Atlantis"},
+        new SpaceShuttle() {UID = System.Guid.NewGuid().ToString(), Name = "Endeavor"},
+        new SpaceShuttle() {UID = System.Guid.NewGuid().ToString(), Name = "Enterprise"}
+      };
+      return list;
+    }
+  }
+}
---
+++ domain/SpaceShuttle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Space.Domain
+{
+  public class SpaceShuttle
+  {
+    public string UID { get; set; }
+    public string Name { get; set; }
+  }
+}
